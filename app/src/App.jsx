e208b2f327f0770e91badd533c67c94216f74efa
Tariq Smith <tariqanthonysmith@gmail.com>
2023-04-17 17:38:19 +0100

    Added user interaction

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,29 +1,76 @@
 import './App.css'
 
+const EventExample = () => {
+
+  const handleFormInput = (event) => {
+    console.log(event.target.value, event.target.name);
+  }
+
+  const handleClick = (event) => {
+    console.log("Button Clicked!", event.target)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted", e);
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h2>Some Form</h2>
+        <input
+        type='text'
+        name='example'
+        onChange={(e) => {console.log(e.target.value)}}
+        style={{margin: "1rem 0"}} />
+        <button type='submit'>Submit Form</button>
+      </form>
+      <button onClick={handleClick}>Click Me!</button>
+    </section>
+  )
+}
+
+const bookArray = [{
+  img: "https://m.media-amazon.com/images/I/512cU+NhyAL._SX314_BO1,204,203,200_.jpg",
+  title: "Lessons in Chemistry",
+  author: "Bonnie Garmus",
+  id: 1
+},
+{
+  img: "https://m.media-amazon.com/images/I/4138h5XntrL._SX218_BO1,204,203,200_QL40_ML2_.jpg",
+  title: "The Boy, The Mole, The Fox and The Horse",
+  author: "Charlie Mackesy",
+  id: 2
+}]
+
 function App() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <EventExample/>
+      {bookArray.map((book) => {
+        const { img, title, author, id} = book;
+        return (
+          <Book
+            key={id}
+            img={img}
+            title={title} 
+            author={author}/>
+        )
+      })}
     </section>
   );
 }
 
-function Book() {
+function Book({ img, title, author, children}) {
+
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title}/>
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 }
-
-const Image = () => <img src='https://m.media-amazon.com/images/I/512cU+NhyAL._SX314_BO1,204,203,200_.jpg' alt='Lessons in Chemistry, Bonnnie Garmus'/>;
-const Title = () => <h2>Lessons in Chemistry</h2>
-const Author =() => <h4>Bonnie Garmus</h4>
 
 export default App
